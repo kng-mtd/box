512ca6212c143b85bad646408f1cf836b0b0b77d
kng-mtd <79887414+kng-mtd@users.noreply.github.com>
2025-03-21 05:54:13 +0900

    Add files via upload

---
+++ svd/PCAvsSVD.R
@@ -0,0 +1,45 @@
+tb0=penguins |> drop_na()|> select(is.numeric,-year)
+tb=scale(tb0)
+
+#covariance matrix tXX/(n-1)
+cov(tb0)
+
+#correlation matrix tXnXn, Xn=scale(X)
+cor(tb0)
+cov(tb)
+
+
+#variance: eigen values of covariance matrix
+eigen(cov(tb))$values
+
+prcomp(tb)$sdev**2
+
+svd(tb)$d**2/(nrow(tb)-1)
+
+library(rsvd)
+rsvd(tb,k=2)$d**2/(nrow(tb)-1)
+
+
+#loadings: eigen vector of covariance matrix
+eigen(cov(tb))$vectors
+
+prcomp(tb)$rotation
+
+svd(tb)$v
+svd(tb,nv=2)$v
+
+rsvd(tb,k=2)$v
+
+
+#principal score
+(tb %*% eigen(cov(tb))$vectors)[1:5,]
+
+prcomp(tb)$x[1:5,]
+
+(tb %*% svd(tb)$v)[1:5,]
+
+(svd(tb)$u %*% diag(svd(tb)$d))[1:5,]
+
+(tb %*% rsvd(tb,k=2)$v)[1:5,]
+
+(rsvd(tb,k=2)$u %*% diag(rsvd(tb,k=2)$d))[1:5,]
