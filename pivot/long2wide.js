fc9d56253c89bf682d0023afb2916320fb44c0ca
kng-mtd <79887414+kng-mtd@users.noreply.github.com>
2025-06-03 10:14:08 +0900

    Add files via upload

--- pivot/long2wide.js
+++ pivot/long2wide.js
@@ -3,8 +3,8 @@ var fso = new ActiveXObject("Scripting.FileSystemObject");
 
 // コマンドライン引数の処理
 var args = WScript.Arguments;
-var inputFile = args.length > 0 ? args(0) : "wide.csv";
-var outputFile = args.length > 1 ? args(1) : "long.csv";
+var inputFile = args.length > 0 ? args(0) : "long.csv";
+var outputFile = args.length > 1 ? args(1) : "wide.csv";
 
 // CSVファイルの読み込み
 function readCSV(filePath) {
@@ -29,19 +29,47 @@ function writeCSV(filePath, data) {
     file.Close();
 }
 
-// 横持ちから縦持ちに変換
+// 縦持ちから横持ちに変換
 function transformCSV(inputData) {
     var rows = inputData;
-    var headers = rows[0].split(","); // ヘッダー行を分割
-    var result = ["id,x,val"]; // 出力用のヘッダー
+    var headers = {}; // 動的にヘッダーを収集
+    var dataMap = {}; // idごとのデータを保持
+    var result = [];
 
-    // 各行を処理
+    // 1行目はヘッダー: id, x, val
     for (var i = 1; i < rows.length; i++) {
         var cols = rows[i].split(",");
         var id = cols[0];
-        for (var j = 1; j < cols.length; j++) {
-            result.push(id + "," + headers[j] + "," + cols[j]);
+        var key = cols[1];
+        var value = cols[2];
+
+        // データをマッピング
+        if (!dataMap[id]) {
+            dataMap[id] = {};
         }
+        dataMap[id][key] = value;
+
+        // ヘッダーを記録
+        if (!headers[key]) {
+            headers[key] = true;
+        }
+    }
+
+    // ヘッダー行の作成
+    var headerRow = ["id"];
+    for (var key in headers) {
+        headerRow.push(key);
+    }
+    result.push(headerRow.join(","));
+
+    // 各idごとのデータ行を作成
+    for (var id in dataMap) {
+        var row = [id];
+        for (var j = 1; j < headerRow.length; j++) {
+            var colName = headerRow[j];
+            row.push(dataMap[id][colName] || ""); // 値が存在しない場合は空文字
+        }
+        result.push(row.join(","));
     }
 
     return result;
